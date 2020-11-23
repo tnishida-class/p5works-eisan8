a9b48f00e079fa35c97a017db28d4c4f98fb51c8
eisan8 <i.eito@icloud.com>
2020-11-23 10:38:38 +0900

    section3-3 ver.2

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,4 +1,4 @@
-// テキスト「関数を作る(2) 値を戻す関数」～「総仕上げ：カレンダーを描画しよう」
+// テキスト「関数を作る 値を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
   createCanvas(200, 200);
   calendar(2019, 10);
@@ -14,19 +14,43 @@ function setup(){
 }
 
 function calendar(y, m){
+
+  let size = width/7
+  let n = 0
+ for (let i = 0; i <= 5; i++){
+   for(let t =0; t <=6; t++){
+
+ rect(size*t,size*i,size,size);
+
   let dow = dayOfWeek(y, m, 1);
   for(let d = 1; d <= daysInMonth(y, m); d++){
+
+    textSize(5);
+    textFont("serif");
+    text("d", dow*width+3, n*width+3);
+    if(dow=6){
+      n += 1;
+    }
+  }
+
+
+
+  //  rect( dayOfWeek(y,m,d)*size , n*size , size , size);
+
+
+ }  }
     // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
   }
-}
 
 function isLeapYear(y){
   return (y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0);
 }
 
 function daysInYear(y){
-  // BLANK[1]
+
+return isLeapYear(y) ? 366 : 355;
 }
+
 
 function daysInMonth(y, m){
   if(m == 2){
@@ -49,7 +73,14 @@ function dayOfYear(y, m, d){
 }
 
 function dayOfWeek(y, m, d){
-  // BLANK[2]
+
+let c =dayOfYear(y,m,d);
+let e =Math.floor((y-2000)/4);
+let f =(e*2+2+(y-2001-e))%7;
+let a =(c+f+5)%7; //
+
+console.log(a) ;
+    // BLANK[2]
 }
 
 function dayOfWeekAsString(dow){
