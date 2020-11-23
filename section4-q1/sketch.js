7ef58366bbe0f9040026a1432244da51e7f955e5
eisan8 <i.eito@icloud.com>
2020-11-23 10:32:55 +0900

    4-q1 and 4-q2 ver.2

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,6 +1,8 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
+//let n;
 
 function setup(){
   createCanvas(200, 200);
@@ -8,9 +10,22 @@ function setup(){
   cycle = 100;
 }
 
+
 function draw(){
   background(160, 192, 255);
+
   count = (count + 1) % cycle;
+  if(count>49){
+    size=50-(count-50);}
+
+ else{size = count;}
+
+
+
+//function KeyPressed(){
+//  if(key == " "){ n=2;}
+//else{n=1;}}
+
   // BLANK[1]
   ellipse(width / 2, height / 2, size);
 }
--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,18 +1,25 @@
 // テキスト「アニメーション」
 let x, y, vx, vy;
-
+let angle;
+let count;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
   vx = 8;
   vy = 8;
+  count=0;
+
 }
 
 function draw(){
   background(160, 192, 255);
-  // BLANK[2] (hint: 作った star 関数を使います)
 
+star(x,y,40,10)
+x += vx;
+y += vy;
+
+//blank
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -23,13 +30,15 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI+angle+count;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
   }
   endShape(CLOSE);
+  count=(count+10);
+
 }
 
 function windowResized(){
